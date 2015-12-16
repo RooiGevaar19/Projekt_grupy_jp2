ec4460f1dc9bfc7ead34145f965315e103c6d3ed
Paweł Lipkowski <plipkowski@p110-13.labpk.inf.ug.edu.pl>
2015-12-16 09:02:12 +0100

    j

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int szesciokat_foremny(int a);
+int trojkat(int a, int h);
+int prostokat(int a, int b);
+int trapez (int a, int b, int h);
+
+int main()
+{
+	int command;
+	printf("Obliczanie pola !");
+	printf("\nCo za pole chcesz obliczyc ?");
+	printf("\n1 - szesciokata foremnego ?");
+	printf("\n2 - trojkata ?");
+	printf("\n3 - prostokata ?");
+	printf("\n4 - trapezu ?\n");
+	scanf("%d", command);
+	switch (command)
+	{
+		1 : {
+		//
+		}
+	}	
+	return 0;
+}
+
